4f3dae28561d144a8cf31fabec7fcbcde93e3533
Michael F <mfriedmanzz01@gmail.com>
2015-02-03 16:31:13 -0700

    all

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).on('ready', function() {
+ 
+ // Palindrome Function
+
+ var string = "racecar";
+
+	var palindrome = function(string) {
+
+	};
+
+	console.log("Is this a Palindrome? ", palindrome(string)); 
+
+
+});
